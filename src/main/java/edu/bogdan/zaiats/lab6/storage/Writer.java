4077171553dc70285faeb78b313ac1bbd75fd09b
zayats1 <zaiats.bogdan@gmail.com>
2024-11-14 12:21:28 +0200

    Writer preview

---
+++ src/main/java/edu/bogdan/zaiats/lab6/storage/Writer.java
@@ -0,0 +1,21 @@
+package edu.bogdan.zaiats.lab6.storage;
+
+import edu.bogdan.zaiats.lab6.music.Music;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+
+public class Writer {
+    public boolean write(String filename, String data)  {
+        try {
+            var writer =  new FileWriter(filename);
+            writer.write("sss");
+        } catch (IOException e){
+            System.out.println(e.toString());
+            return  false;
+        }
+
+        return true;
+    }
+}
