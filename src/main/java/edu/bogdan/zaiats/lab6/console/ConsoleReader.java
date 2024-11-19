ed5fdcc6985d3e14adb62312fd0aa9c4f207eb60
zayats1 <zaiats.bogdan@gmail.com>
2024-11-19 23:17:14 +0200

    introduce console Reader

---
+++ src/main/java/edu/bogdan/zaiats/lab6/console/ConsoleReader.java
@@ -0,0 +1,10 @@
+package edu.bogdan.zaiats.lab6.console;
+
+import java.util.Optional;
+
+public class ConsoleReader {
+    static Optional<String> read(){
+
+        return Optional.empty();
+    }
+}
