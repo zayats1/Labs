61fcd011c53c1d65fdb9d4eb54e09dd1283e2486
zayats1 <zaiats.bogdan@gmail.com>
2024-11-20 11:15:39 +0200

    Rename ConsoleReader to MusicReader

--- src/main/java/edu/bogdan/zaiats/lab6/console/MusicReader.java
+++ src/main/java/edu/bogdan/zaiats/lab6/console/MusicReader.java
@@ -1,9 +1,11 @@
 package edu.bogdan.zaiats.lab6.console;
 
+import edu.bogdan.zaiats.lab6.music.Music;
+
 import java.util.Optional;
 
-public class ConsoleReader {
-    static Optional<String> read(){
+public class MusicReader {
+    static Optional<Music> read(){
 
         return Optional.empty();
     }
