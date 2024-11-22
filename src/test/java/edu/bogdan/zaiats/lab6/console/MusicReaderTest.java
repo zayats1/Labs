b62e53d2354d82925042af88e71004aab023b7b4
zayats1 <zaiats.bogdan@gmail.com>
2024-11-22 12:51:32 +0200

    clean up tests

--- src/test/java/edu/bogdan/zaiats/lab6/console/MusicReaderTest.java
+++ src/test/java/edu/bogdan/zaiats/lab6/console/MusicReaderTest.java
@@ -1,6 +1,8 @@
 package edu.bogdan.zaiats.lab6.console;
 
 import edu.bogdan.zaiats.lab6.music.Music;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -13,20 +15,26 @@ import static org.junit.jupiter.api.Assertions.*;
 class MusicReaderTest {
     private final InputStream systemIn = System.in;
     private final PrintStream systemOut = System.out;
+
+    @BeforeEach
+    void before() {
+        var data = "Foreword\nMeteora\nLinkin park\n0:13\nrock\n";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        System.setOut(new PrintStream(new ByteArrayOutputStream()));
+    }
+
+    @AfterEach
+    void after() {
+        System.setIn(System.in);
+        System.setOut(System.out);
+    }
+
     @Test
     void read() {
-        try {
-            var data = "Foreword\nMeteora\nLinkin park\n0:13\nrock\n";
-            System.setIn(new ByteArrayInputStream(data.getBytes()));
-            System.setOut(new PrintStream(new ByteArrayOutputStream()));
-            var music  = MusicReader.read(System.in);
-            assertTrue(music.isPresent());
-            assertEquals(new Music(
-                    "Foreword","Meteora","Linkin park","0:13","rock"
-            ),music.get());
-        } finally {
-            System.setIn(System.in);
-            System.setOut(System.out);
-        }
+        var music = MusicReader.read(System.in);
+        assertTrue(music.isPresent());
+        assertEquals(new Music(
+                "Foreword", "Meteora", "Linkin park", "0:13", "rock"
+        ), music.get());
     }
 }
