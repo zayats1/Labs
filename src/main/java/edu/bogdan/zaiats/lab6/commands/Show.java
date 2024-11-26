f79120322204bdbe8688e68d1f7c36157baa1797
zayats1 <zaiats.bogdan@gmail.com>
2024-11-27 00:01:22 +0200

    Rename read command to show

--- src/main/java/edu/bogdan/zaiats/lab6/commands/Show.java
+++ src/main/java/edu/bogdan/zaiats/lab6/commands/Show.java
@@ -2,13 +2,13 @@ package edu.bogdan.zaiats.lab6.commands;
 
 import edu.bogdan.zaiats.lab6.service.MusicService;
 
-public class Read extends MusicServiceCommand {
-    public Read(MusicService service) {
+public class Show extends MusicServiceCommand {
+    public Show(MusicService service) {
         super(service);
     }
 
     @Override
     public void execute() {
-        this.service.read();
+        this.service.show();
     }
 }
