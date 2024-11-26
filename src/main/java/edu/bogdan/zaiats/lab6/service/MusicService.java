9769d635d8c685429d63c0722387d5180ab43b07
zayats1 <zaiats.bogdan@gmail.com>
2024-11-26 22:44:21 +0200

    add more functional(Raw)

--- src/main/java/edu/bogdan/zaiats/lab6/service/MusicService.java
+++ src/main/java/edu/bogdan/zaiats/lab6/service/MusicService.java
@@ -1,8 +1,13 @@
 package edu.bogdan.zaiats.lab6.service;
 
 import edu.bogdan.zaiats.lab6.console.MusicReader;
+import edu.bogdan.zaiats.lab6.csv.MusicCsvParser;
 import edu.bogdan.zaiats.lab6.music.Music;
+import edu.bogdan.zaiats.lab6.storage.Reader;
+import edu.bogdan.zaiats.lab6.storage.Writer;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,4 +28,36 @@ public class MusicService {
        }
        System.out.println(records);
    }
+
+
+     public void load(){
+        try {
+            var data = Reader.readFile("saved.csv");
+
+        for (var line : data){
+              var music = MusicCsvParser.fromLine(line);
+              if (music != null && !records.contains(music)){
+                  records.add(music);
+              }
+            }
+
+        } catch (FileNotFoundException e) {
+           System.out.println("No file");
+        }
+     }
+
+
+    public void save() {
+        List<String> data = records.stream().map(Music::toString).toList();
+        try {
+            Writer.write("saved.csv",String.join("\n",data));
+        } catch (IOException e) {
+            System.out.println("Something went wrong while saving the file");
+            System.out.println(e.toString());
+        }
+    }
+
+    public void removeRecord() {
+
+    }
 }
