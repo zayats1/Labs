267d6ae6a6dfd352f09f9bb32bda3bd34f353ed5
zayats1 <zaiats.bogdan@gmail.com>
2024-11-26 22:00:04 +0200

    Major rewrite of menu

--- src/test/java/edu/bogdan/zaiats/lab6/console/MenuTest.java
+++ src/test/java/edu/bogdan/zaiats/lab6/console/MenuTest.java
@@ -1,17 +1,25 @@
 package edu.bogdan.zaiats.lab6.console;
 
 import edu.bogdan.zaiats.lab6.commands.Add;
-import edu.bogdan.zaiats.lab6.commands.Read;
-import org.junit.jupiter.api.Test;
 import edu.bogdan.zaiats.lab6.service.MusicService;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuTest {
 
     @Test
+    void help() {
+        var menu = new Menu();
+        var help = menu.help();
+        assertEquals("Available Commands\nQuit\nHelp", help);
+    }
+
+
+    @Test
     void get() {
         var menu = new Menu();
-        var command = menu.get("Help");
+        var command = menu.get("Helps");
         assertFalse(command.isPresent());
 
         var service = new MusicService();
