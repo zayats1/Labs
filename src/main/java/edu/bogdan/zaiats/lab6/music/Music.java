6c064d893b5b920eaed3d6b22155a1c0bc2f45b5
zayats1 <zaiats.bogdan@gmail.com>
2024-11-27 11:22:14 +0200

    remove unnecessary to String in the music record

--- src/main/java/edu/bogdan/zaiats/lab6/music/Music.java
+++ src/main/java/edu/bogdan/zaiats/lab6/music/Music.java
@@ -1,12 +1,4 @@
 package edu.bogdan.zaiats.lab6.music;
 
-public record Music(String name, String album, String author, String duration, String style) {
-    @Override
-    public String toString() {
-        return "name='" + name + '\'' +
-                ", album='" + album + '\'' +
-                ", author='" + author + '\'' +
-                ", duration='" + duration + '\'' +
-                ", style='" + style + '\'';
-    }
-}
+public record Music(String name, String album, String author, String duration, String style){}
+
