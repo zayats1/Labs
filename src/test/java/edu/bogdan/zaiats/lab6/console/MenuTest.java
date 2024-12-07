5afe37cf53e645c74a965173df57cab9568b9340
zayats1 <zaiats.bogdan@gmail.com>
2024-12-07 22:41:32 +0200

    sync My main pc is broken
    Signed-off-by: zayats1 <zaiats.bogdan@gmail.com>

--- src/test/java/edu/bogdan/zaiats/lab6/console/MenuTest.java
+++ src/test/java/edu/bogdan/zaiats/lab6/console/MenuTest.java
@@ -1,13 +1,38 @@
 package edu.bogdan.zaiats.lab6.console;
 
-import edu.bogdan.zaiats.lab6.commands.Add;
+import edu.bogdan.zaiats.lab6.commands.*;
 import edu.bogdan.zaiats.lab6.service.MusicService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MenuTest {
+    private final ByteArrayOutputStream printRes = new ByteArrayOutputStream();
+    @BeforeEach
+    void before() {
+        try {
+            printRes.flush();
+        } catch (IOException e) {
+            System.out.println("Done");
+        }
+        var data = "Foreword\nMeteora\nLinkin park\n0:13\nrock\n";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        System.setOut(new PrintStream(printRes));
+    }
 
+    @AfterEach
+    void after(){
+        System.setIn(System.in);
+        System.setOut(System.out);
+    }
     @Test
     void help() {
         var menu = new Menu();
@@ -31,5 +56,51 @@ class MenuTest {
         var addCommand = newMenu.get("Add");
         assertTrue(addCommand.isPresent());
         assertEquals(addCommand.get().getName(), Add.class.getSimpleName());
+    }
+
+    @Test
+    void show() {
+        var service = new MusicService();
+        var menu = new Menu(
+                new Show(service),
+                new Add(service),
+                new Save(service),
+                new Load(service),
+                new Remove(service)
+        );
+
+        var data = """
+                Help
+                Quit
+                """;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        var console = new Scanner(System.in);
+        menu.show(console,
+                new PrintStream(printRes));
+        var expected = """
+                Available Commands
+                Quit
+                Help
+                Show
+                Add
+                Save
+                Load
+                Remove
+                
+                Try again
+                 To see commands type Help
+                Help
+                Available Commands
+                Quit
+                Help
+                Show
+                Add
+                Save
+                Load
+                Remove
+                Quit
+                """;
+        var out = printRes.toString();
+        assertEquals(expected, out);
     }
 }
